8878592119ee12c956536d6e8292739c880342b7
Karanbir Singh <krnbr@live.in>
2018-08-22 21:17:58 +0530

    passport

--- app.js
+++ app.js
@@ -6,13 +6,14 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
-
 
 const mongoose = require('mongoose');
 mongoose.promise = require('bluebird');
@@ -45,27 +46,23 @@ app.use(session({
     resave: false,
     store: new FileStore()
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
-    console.log(req.session);
+    console.log(req.user);
 
-    if(!req.session.user) {
+    if (!req.user) {
         var err = new Error('You are not authenticated!');
-        err.status = 403;
-        return next(err);
+        res.setHeader('WWW-Authenticate', 'Basic');
+        err.status = 401;
+        next(err);
     }
     else {
-        if (req.session.user === 'authenticated') {
-            next();
-        }
-        else {
-            var err = new Error('You are not authenticated!');
-            err.status = 403;
-            return next(err);
-        }
+        next();
     }
 }
 
