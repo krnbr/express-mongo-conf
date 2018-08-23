a14c4f1bc95103adfd8352fc78b8c3c829ced45f
Karanbir Singh <krnbr@live.in>
2018-08-23 08:08:41 +0530

    Added some initial structure

--- authenticate.js
+++ authenticate.js
@@ -37,3 +37,14 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
     }));
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function (req, res, next) {
+    console.log("--- "+req.user.admin)
+    if(req.user.admin){
+        next();
+    }else{
+        var err = new Error('You are forbidden to perform this function!');
+        err.status = 403;
+        return next(err);
+    }
+}
